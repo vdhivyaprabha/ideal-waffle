bddc2011367d51906dbdd6298a36fe20bc3a1672
Dhivyaprabha Vijayakumaran <vdhivyaprabha@gmail.com>
2023-04-16 12:54:59 +0530

    Developed Rubby Snippets for demonstatting a while statement

--- 02-RubyPlayGround/01-ruby-working-samples/05-IterationOf-Contents.rb
+++ 02-RubyPlayGround/01-ruby-working-samples/05-IterationOf-Contents.rb
@@ -12,23 +12,36 @@ class IterationOfContends
         for indv_element in 1..@@max_count do
             puts("The index is at the element #{indv_element}")
         end
-
         # Iterate prededined array and display where the index is while iterating
-        
         for indv_element in potter_array do
             puts("The index is at the element #{indv_element}")
         end
     end
 
+    def iterate_using_whiledowhile(potter_array)
+        array_length = potter_array.length()
+        index = 0
+        while index < array_length
+            element = potter_array.at(index)
+            puts("The value at the index :: using index :: #{index} is #{potter_array[index]}")
+            puts("The value at the index :: using Arrays.at :: #{index} is #{element}")
+            index+=1
+        end
+    end
+
 end
+
+potter_array = ["The Black Family","The Bones family","The Crouch family","The Diggory family","The Gaunt family","The Longbottom family","The Malfoy family","The Potter family","The Weasley family","The Dursley family"]
 
 # Get an input number from the user and iterate it untill the maximum number.
 # restrict the maximum number to 10
-potter_array = ["The Black Family","The Bones family","The Crouch family","The Diggory family","The Gaunt family","The Longbottom family","The Malfoy family","The Potter family","The Weasley family","The Dursley family"]
 puts("Enter the Maximum Number till which the iteration should continue :: ")
 max_count = gets.chomp.to_i
 if max_count > 10
     max_count = 10
 end
+
 iteration_instance = IterationOfContends.new(max_count)
 iteration_instance.iterate_using_forloop(potter_array)
+
+iteration_instance.iterate_using_whiledowhile(potter_array)
